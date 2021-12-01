bd3845c8a279eda10fac454439ce131ab33a4061
spencerporter <scfp419@gmail.com>
2021-11-30 18:13:17 -0700

    Adding Build Folder

---
+++ build/precache-manifest.50c91d2a9c48535528a038c6caa76ced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073f6418213610ed7ef39581252888d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ce667d28d5c8f57ff0",
+    "url": "/static/js/2.37d46fee.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.37d46fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833446cbfbbf371b12fa",
+    "url": "/static/js/main.7a101af3.chunk.js"
+  },
+  {
+    "revision": "6fa31919e672cdd17db0",
+    "url": "/static/js/runtime-main.9df7520e.js"
+  }
+]);
